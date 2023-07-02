1ce00909da428ef9766c7822845145ab18e4e60c
ilyasTraikia <88138221+ilyasTraikia@users.noreply.github.com>
2023-07-02 22:36:53 +0100

    Update destroy.jsx

--- src/routes/destroy.jsx
+++ src/routes/destroy.jsx
@@ -4,6 +4,6 @@ import { redirect } from "react-router-dom";
 
 export async function action({ params }) {
 
-  await axios.delete(`http://localhost:4000/tasks/deleteTask/${params.taskId}`);
+  await axios.delete(`https://task-backend-api.onrender.com/tasks/deleteTask/${params.taskId}`);
   return redirect("/board");
 }
